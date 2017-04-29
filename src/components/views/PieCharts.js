696afe01d18a4c7bbf9f504b7d2b5a2b3774eed6
superhero2017 <super.hero2017@outlook.com>
2017-04-29 21:22:19 +0200

    #7 No data

--- src/components/views/PieCharts.js
+++ src/components/views/PieCharts.js
@@ -5,6 +5,8 @@ import Bar from '../charts/Bar'
 import DashboardSummary from '../layout/DashboardSummary'
 import PageFooter from '../layout/PageFooter'
 
+import services from 'src/modules/services';
+
 export default {
   name: 'piecharts',
   template: require('components/views/PieCharts.html'),
@@ -18,7 +20,27 @@ export default {
   },
   data () {
     return {
-      model: nch.model
+      model: nch.model,
+      dataflag: false
     }
+  },
+  computed: {
+    dataflag() {
+      return this.dataflag;
+    }
+  },
+  mounted() {
+    console.log( "Pie Charts mounted" );
+    services.getPieData().then( (response) => {
+      for (var i = 0; i < response.length; i++) {
+        for (var j = 0; j < this.model.selectedCategories.length; j++) {
+          if(this.model.selectedCategories[j] == response[i].categoryname) {
+            this.dataflag = true
+            break
+          }
+        }
+      }
+      
+    }).catch((message) => { console.log('Pie promise catch:' + message) })
   }
 }
