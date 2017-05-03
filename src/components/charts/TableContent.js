87b12f60d41edefe0d7c4befe0dd158c6186a05f
Louis Austin <louis.m.austin@gmail.com>
2017-05-03 16:50:26 -0300

    fixed

--- src/components/charts/TableContent.js
+++ src/components/charts/TableContent.js
@@ -13,28 +13,24 @@ export default {
     return {
       media: nch.model.allMedaiNames,
       currentManufacturer: nch.model.currentManufacturer,
-      compareManufacturer: "Comparables"
+      compareManufacturer: 'Comparables'
     }
   },
   methods: {
-    formattedValue( value ) {
-      return d3.format("(.2f")(value)
+    formattedValue (value) {
+      return d3.format('(.2f')(value)
     },
-    getIcon( mediaValue ) {
-      if( mediaValue == "FSI" ) {
-        return "fa-scissors";
-      }
-      else if( mediaValue == "Print At Home" ) {
-        return "fa-print";
-      }
-      else if( mediaValue == "Handout Electronic Checkout" ) {
-        return "fa-tablet";
-      }
-      else if( mediaValue == "Military" ) {
-        return "fa-star";
-      }
-      else {
-        return "fa-question-circle";
+    getIcon (mediaValue) {
+      if (mediaValue === 'FSI') {
+        return 'fa-scissors'
+      } else if (mediaValue === 'Print At Home') {
+        return 'fa-print'
+      } else if (mediaValue === 'Handout Electronic Checkout') {
+        return 'fa-tablet'
+      } else if (mediaValue === 'Military') {
+        return 'fa-star'
+      } else {
+        return 'fa-question-circle'
       }
     }
   }
