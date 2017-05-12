4cb7e6e8833c8f6857e2de2390acb31da250a1e0
superhero2017 <super.hero2017@outlook.com>
2017-05-12 14:23:28 +0200

    Almost completed

--- src/components/charts/StackedBar.js
+++ src/components/charts/StackedBar.js
@@ -4,6 +4,7 @@ import services from 'src/modules/services'
 
 export default {
   name: 'stackedbar',
+  props: ['groupByField', 'labelField'],
   template: require('components/charts/StackedBar.html'),
   data () {
     return {
@@ -22,63 +23,50 @@ export default {
 
   mounted() {
     console.log('Stacked Bar mounted: ')
-    services.getStackedBarChartData().then((response) => {
-      this.stackedData = response
-      this.render()
-    }).catch((message) => {
-      console.log('Stacked Bar promise catch:' + message)
-    })
+    if(this.groupByField == 'facevalue')
+    {
+      services.getStackedBarChartData().then((response) => {
+        this.stackedData = response
+        this.render()
+      }).catch((message) => {
+        console.log('Stacked Bar promise catch:' + message)
+      })
+    }
+    else if(this.groupByField == 'productmoved')
+    {
+      services.getProductMovedPieData().then((response) => {
+        this.stackedData = response
+        this.render()
+      }).catch((message) => {
+        console.log('Stacked Bar promise catch:' + message)
+      })
+    }
   },
   methods: {
 
     render() {
+      var items = this.stackedData
+      if(items.length == 0)
+        return
 
-      var data = []
-      for (var i = 0; i < this.stackedData.length; i++) {
-        for (var j = 0; j < data.length; j++) {
-          if((data[j].mfrname == this.stackedData[i].mfrname) && (this.model.selectedMedia.value == '' || this.model.selectedMedia.value == this.stackedData[i].medianame))
-          {
+      var responseData = []
+      var groupBy = this.groupByField
 
-            if(this.stackedData[i].price < 0.4)
-              data[j]['< $0.40'] += this.stackedData[i].totalcouponredemption
-            else if(this.stackedData[i].price <= 0.75)
-              data[j]['$0.40 - $0.75'] += this.stackedData[i].totalcouponredemption
-            else if(this.stackedData[i].price <= 1)
-              data[j]['$0.76 - $1.00'] += this.stackedData[i].totalcouponredemption
-            else
-              data[j]['$1.00 +'] += this.stackedData[i].totalcouponredemption
-            break
-          }
-        }
-        if((j == data.length) && (this.model.selectedMedia.value == '' || this.model.selectedMedia.value == this.stackedData[i].medianame))
-        {
-          var newItem = {
-            'mfrname' : this.stackedData[i].mfrname,
-            '< $0.40' : 0,
-            '$0.40 - $0.75' : 0,
-            '$0.76 - $1.00' : 0,
-            '$1.00 +' : 0
-          }
-          if(this.stackedData[i].price < 0.4)
-            newItem['< $0.40'] += this.stackedData[i].totalcouponredemption
-          else if(this.stackedData[i].price <= 0.75)
-            newItem['$0.40 - $0.75'] += this.stackedData[i].totalcouponredemption
-          else if(this.stackedData[i].price <= 1)
-            newItem['$0.76 - $1.00'] += this.stackedData[i].totalcouponredemption
-          else
-            newItem['$1.00 +'] += this.stackedData[i].totalcouponredemption
-          data.push(newItem)
-        }
+      if(groupBy == 'facevalue') {
+        responseData = this.getDataForMediaTypes(items, this.model.selectedMedia.value)
+      }
+      else if(groupBy == 'productmoved') {
+        responseData = this.getDataForProductMoved(items, this.model.selectedProductMoved.value)
       }
 
-      var svg = d3.select('#stackedBarChart').attr('width', 600).html(''),
+      var svg = d3.select('#stackedBarChart').attr('width', 700).html(''),
         margin = {top: 20, right: 20, bottom: 30, left: 20},
         width = +svg.attr('width') - margin.left - margin.right,
         height = +svg.attr('height') - margin.top - margin.bottom,
         g = svg.append('g').attr('transform', 'translate(' + 200 + ',' + margin.top + ')')
 
       var x = d3.scaleBand()
-        .rangeRound([0, width - 300])
+        .rangeRound([0, width - 400])
         .paddingInner(0.05)
         .align(0.1)
 
@@ -86,28 +74,38 @@ export default {
         .rangeRound([0, height - 80])
 
       var z = d3.scaleOrdinal()
-        .range(['#927DB2', '#AAC66C', '#CE6660', '#5B90C6'])
+        .range(['#5B90C6', '#CE6660', '#AAC66C', '#927DB2'])
 
-
-      var keys = Object.keys(data[0]).slice(1)
-
-      for (var i = 0; i < data.length; i++) {
-        var t = 0
-        for (var j in data[i]) {
-
-          if(j != 'mfrname')
-            t += data[i][j]
-        }
-        for (var j in data[i]) {
-          if(j != 'mfrname')
-            data[i][j]= data[i][j] / t * 100
-        }
-        data[i].total = 100
+      var keys 
+      if(groupBy == 'facevalue') {
+        keys= ['$1.00 +', '$0.76 - $1.00', '$0.40 - $0.75', '< $0.40']
+      }
+      else if(groupBy == 'productmoved') {
+        keys = Object.keys(responseData[0]).slice(1)
       }
 
-      data.sort(function(a, b) { return b.total - a.total })
-      x.domain(data.map(function(d) { return d.mfrname }))
-      y.domain([0, d3.max(data, function(d) { return d.total })]).nice()
+      var length = keys.length
+
+      for (var i = 0; i < responseData.length; i++) {
+        var t = 0
+        for (var j in responseData[i]) {
+
+          if(j != 'mfrname')
+            t += responseData[i][j]
+        }
+        if(groupBy == 'facevalue') {
+          for (var j in responseData[i]) {
+            if(j != 'mfrname')
+              responseData[i][j]= responseData[i][j] / t * 100
+          }
+          t = 100
+        }
+        responseData[i].total = t
+      }
+
+      responseData.sort(function(a, b) { return b.total - a.total })
+      x.domain(responseData.map(function(d) { return d.mfrname }))
+      y.domain([d3.max(responseData, function(d) { return d.total }),0]).nice()
       z.domain(keys)
 
       // filters go in defs element
@@ -132,7 +130,7 @@ export default {
       filter.append('feOffset')
           .attr('in', 'blur')
           .attr('dx', 1)
-          .attr('dy', 2)
+          .attr('dy', 0)
           .attr('result', 'offsetBlur')
 
       // overlay original SourceGraphic over translated blurred opacity by using
@@ -144,56 +142,83 @@ export default {
       feMerge.append('feMergeNode')
           .attr('in', 'SourceGraphic')
 
+      if(groupBy == 'productmoved') {
+        var axisData = y.ticks()
+
+        g.append('g')
+          .selectAll('g')
+          .data(axisData)
+          .enter().append('line')
+          .attr('y1', function (d) { return y(d) })
+          .attr('y2', function (d) { return y(d) })
+          .attr('x1', 70)
+          .attr('x2', 470 )
+          .attr('stroke', 'grey')
+          .style('stroke-dasharray','5, 5')
+
+        g.append('g')
+          .selectAll('g')
+          .data(axisData)
+          .enter().append('text')
+          .attr('y', function (d) { return y(d) + 5 })
+          .attr('x', 50)
+          .text(function(d) { return d })
+          .style('text-anchor', 'end')
+          .attr('fill', 'grey')
+      }
+
       g.append('g')
         .selectAll('g')
-        .data(d3.stack().keys(keys)(data))
+        .data(d3.stack().keys(keys)(responseData))
         .enter().append('g')
         .attr('class','oneRect')
         .attr('fill', function(d) { return z(d.key) })
         .selectAll('rect')
         .data(function(d) { return d })
         .enter().append('rect')
-        .attr('x', function(d, i) {
-          return x(d.data.mfrname) + i * 50 })
-        .attr('y', function(d) { return y(d[0]) })
-        .attr('height', function(d) { return y(d[1]- d[0]) })
+        .attr('x', function(d, i) { return x(d.data.mfrname) + i * 50 + 120 })
+        .attr('y', function(d) {
+          return y(d[1]) 
+        })
+        .attr('height', function(d) { 
+          return  (((height - 80 - (y(d[1]- d[0])) > 3)?(height - 83 - (y(d[1]- d[0]))):(0)) )
+        })
         .attr('width', x.bandwidth())
         .attr('stroke', 'white')
-        .attr('stroke-width', '2px')
+        .attr('stroke-width', '2')
         .style('filter', 'url(#drop-shadow)')
 
-      g.selectAll('.oneRect')
-        .data(d3.stack().keys(keys)(data))
+      g.append('g')
+        .selectAll('g')
+        .data(d3.stack().keys(keys)(responseData))
+        .enter().append('g')
         .selectAll('g')
         .data(function(d) { return d })
         .enter().append('text')
-        .attr('x', function (d, i) {
-          return x.bandwidth()/2 + x(d.data.mfrname) + i * 50
-        })
+        .attr('x', function (d, i) { return x.bandwidth()/2 + x(d.data.mfrname) + i * 50 + 120 })
         .style('text-anchor', 'middle')
         .style('font-weight', 'bold')
         .attr('y', function(d) {
-          return (y(d[0]) + y(d[1]))/2 })
+          return y(d[1]/2 + d[0]/2) 
+        })
         .text(function(d) {
           if((d[1]-d[0])/d.data.total > 0.005)
-            return d3.format('.0%')((d[1]-d[0])/d.data.total)
+            return ((groupBy == 'productmoved')?(d[1]-d[0]):(d3.format('.0%')((d[1]-d[0])/d.data.total)))
           else
             return ''
         })
-
-        .attr('fill','black')
+        .attr('fill','black')      
 
       g.append('g')
         .selectAll('text')
-        .data(data)
+        .data(responseData)
         .enter().append('text')
-        .attr('x', function (d, i) {
-          return x.bandwidth()/2 + x(d.mfrname) + i * 50
-        })
+        .attr('x', function (d, i) { return x.bandwidth()/2 + x(d.mfrname) + i * 50 + 120 })
         .attr('font-weight', 'bold')
         .style('text-anchor', 'middle')
         .attr('y', height - 40)
         .text(function(d) { return d.mfrname })
+
 
       // g.append('g')
       //   .attr('class', 'axis')
@@ -215,13 +240,13 @@ export default {
 
       g.append('g')
         .append('text')
-        .attr('x', 2)
+        .attr('x', 10)
         .attr('y', 0)
         .attr('dy', '0.32em')
         .attr('fill', '#000')
         .attr('font-weight', 'bold')
         .attr('text-anchor', 'start')
-        .text('Face Value Ranges')
+        .text(this.labelField)
 
       var legend = g.append('g')
         .attr('font-family', 'sans-serif')
@@ -232,7 +257,7 @@ export default {
         .data(keys.slice())
         .enter().append('g')
         .attr('transform', function(d, i) {
-          return 'translate(20,' + (40 * i + 20) + ')'
+          return 'translate(20,' + ( 40 * (length - i) - 20) + ')'
         })
 
       legend.append('circle')
@@ -256,7 +281,71 @@ export default {
         .attr('x2', 125)
         .attr('stroke', 'grey')
         .style('stroke-dasharray','5,5')
+    },
+    getDataForMediaTypes(items, selectedMedia) {
+      var responseData = []
+      for (var i = 0; i < items.length; i++) {
+        for (var j = 0; j < responseData.length; j++) {
+          if((responseData[j].mfrname == items[i].mfrname) && (selectedMedia == '' || selectedMedia == items[i].medianame))
+          {
 
+            if(items[i].price < 0.4)
+              responseData[j]['< $0.40'] += items[i].totalcouponredemption
+            else if(items[i].price <= 0.75)
+              responseData[j]['$0.40 - $0.75'] += items[i].totalcouponredemption
+            else if(items[i].price <= 1)
+              responseData[j]['$0.76 - $1.00'] += items[i].totalcouponredemption
+            else
+              responseData[j]['$1.00 +'] += items[i].totalcouponredemption
+            break
+          }
+        }
+        if((j == responseData.length) && (selectedMedia == '' || selectedMedia == items[i].medianame))
+        {
+          var newItem = {
+            'mfrname' : items[i].mfrname,
+            '< $0.40' : 0,
+            '$0.40 - $0.75' : 0,
+            '$0.76 - $1.00' : 0,
+            '$1.00 +' : 0
+          }
+          if(items[i].price < 0.4)
+            newItem['< $0.40'] += items[i].totalcouponredemption
+          else if(items[i].price <= 0.75)
+            newItem['$0.40 - $0.75'] += items[i].totalcouponredemption
+          else if(items[i].price <= 1)
+            newItem['$0.76 - $1.00'] += items[i].totalcouponredemption
+          else
+            newItem['$1.00 +'] += items[i].totalcouponredemption
+          responseData.push(newItem)
+        }
+      }
+      return responseData
+    },
+
+    getDataForProductMoved(items, selectedProductMoved) {
+      var responseData = []
+
+      for (var i = 0; i < items.length; i++) {
+        for (var j = 0; j < responseData.length; j++) {
+          if((responseData[j].mfrname == items[i].mfrname) && (selectedProductMoved== '' || selectedProductMoved == items[i].productmoved)) {
+            if (!(items[i].period in responseData[j])){
+              responseData[j][items[i].period] = 0
+            }
+            responseData[j][items[i].period] += items[i].totalcouponredemption
+            break
+          }
+        }
+
+        if ((j == responseData.length) && (items[i].totalcouponredemption != 0) && (selectedProductMoved== '' || selectedProductMoved == items[i].productmoved)) {
+          var item = {}
+          item['mfrname'] = items[i].mfrname
+          item[items[i].period] = items[i].totalcouponredemption
+          responseData.push(item)
+        }
+      }
+
+      return responseData
     }
   }
 }
