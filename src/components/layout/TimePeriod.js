62808dc8dca054cb7f2083725915c1754ef5dc93
superhero2017 <super.hero2017@outlook.com>
2017-05-17 17:10:45 +0200

    Complete Task

--- src/components/layout/TimePeriod.js
+++ src/components/layout/TimePeriod.js
@@ -11,7 +11,8 @@ export default {
     return {
       model: nch.model,
       showTimePeriodOptions: false,
-      timeperiodData: []
+      timeperiodData: [],
+      scrollValue: 0
     }
   },
   props: {
@@ -19,13 +20,64 @@ export default {
   },
   mounted() {
     services.getTimePeriodData().then( (response) => {
+      response[0].values.sort(function(a, b) { return a.year - b.year })
+      response[1].values.sort(function(a, b) { return b.year - a.year })
+      response[2].values.sort(function(a, b) { return new Date(b.year + b.month) - new Date(a.year + a.month) })
+
+      for(var i = 0; i < response[0].values.length; i++) {
+        response[0].values[i].flag = 1
+      }
+
+      for(var i = 0; i < response[1].values.length; i++) {
+        response[1].values[i].item = []
+        for(var j = 0; j< response[1].values[i].quarters.length; j++) {
+          response[1].values[i].item[j] = {
+            value: response[1].values[i].quarters[j],
+            flag: 1
+          }
+        }
+      }
+
+      for(var i = 0; i < response[2].values.length; i++) {
+        response[2].values[i].item = []
+        for(var j = 0; j< response[2].values[i].weeks.length; j++) {
+          response[2].values[i].item[j] = {
+            value: response[2].values[i].weeks[j],
+            flag: 1
+          }
+        }
+      }
+
       this.timeperiodData = response
-      console.log(this.timeperiodData)
+
     }).catch( (message) => { console.log('TimePeriod promise catch:' + message) })
   },
   methods: {
     onAwayClick: function() {
   	  this.showTimePeriodOptions = false
+    },
+
+    itemClick: function(item) {
+      item.flag = 1 - item.flag
+    },
+
+    itemUp: function() {
+      if(this.scrollValue)
+        this.scrollValue --
+      document.getElementsByClassName('timeperiod-content')[0].scrollTop = 24 + 38 * this.scrollValue
+    },
+
+    itemDown: function() {
+      if(this.scrollValue < this.timeperiodData[this.timeperiodValue]['values'].length - 4)
+        this.scrollValue ++
+      document.getElementsByClassName('timeperiod-content')[0].scrollTop = 24 + 38 * this.scrollValue
+    },
+
+    handleScroll: function(e) {
+      let currentScrollPosition = e.srcElement.scrollTop - 24
+      if( currentScrollPosition < 0)
+        currentScrollPosition = 0
+      this.scrollValue =  Math.ceil (currentScrollPosition / 38)
     }
   }
 }
